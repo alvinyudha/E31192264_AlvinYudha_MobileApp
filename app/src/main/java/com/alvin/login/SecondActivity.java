6840108748fa63b8d9f5ea146cd82e401bcdce08
alvinyudha <alvinyudha73@gmail.com>
2021-04-13 20:29:02 +0700

    Update

--- app/src/main/java/com/alvin/login/SecondActivity.java
+++ app/src/main/java/com/alvin/login/SecondActivity.java
@@ -2,7 +2,10 @@ package com.alvin.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button btnBack = findViewById(R.id.btn_Back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(back);
+            }
+        });
     }
 }
